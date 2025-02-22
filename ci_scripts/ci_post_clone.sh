a05985265ece32c6a4661ee487957a484964d12c
Jérémie Patot <jpatot@Jeremies-MacBook-Air.local>
2025-02-22 22:02:44 +0000

    Fix post clone script shebang 6

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,4 +1,13 @@
 #!/bin/zsh
-brew install tuist
+curl -Ls https://install.tuist.io | bash
+
+export HOMEBREW_CASK_OPTS="--appdir=$HOME/Applications"
+brew install --cask tuist
+
+mkdir -p ~/.tuist
+curl -Ls https://github.com/tuist/tuist/releases/latest/download/tuist.zip -o ~/.tuist/tuist.zip
+unzip ~/.tuist/tuist.zip -d ~/.tuist
+chmod +x ~/.tuist/tuist
+export PATH="$HOME/.tuist:$PATH"
 
 tuist generate
