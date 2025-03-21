e7fb81d1334707ec9d1b47788447ba59a6d7e0d8
Jérémie Patot <jpatot@Jeremies-MacBook-Air.local>
2025-03-21 15:52:17 +0100

    Fix swift version

--- Project.swift
+++ Project.swift
@@ -25,7 +25,7 @@ let project = Project(
                 .package(product: "Helpers"),
             ],
             settings: .settings(base: [
-                "SWIFT_VERSION": " 6.0.2"
+                "SWIFT_VERSION": "6"
             ], configurations: [
                 .debug(name: "Debug", settings: [:]),
                 .release(name: "Release", settings: [:])
