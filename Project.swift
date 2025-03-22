f2e1f7ccae2c6287e4a1da0696de5cac87d1765b
Jérémie Patot <jpatot@Jeremies-MacBook-Air.local>
2025-03-22 10:39:11 +0100

    Fix test unit

--- Project.swift
+++ Project.swift
@@ -25,7 +25,7 @@ let project = Project(
                 .package(product: "Helpers"),
             ],
             settings: .settings(base: [
-                "SWIFT_VERSION": "6"
+                "SWIFT_VERSION": "5"
             ], configurations: [
                 .debug(name: "Debug", settings: [:]),
                 .release(name: "Release", settings: [:])
